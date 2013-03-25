905be5d5d4bc7246b7b5b3178f9d4d2613ee4747
yumehoshi <kimgeraldine_fabe@hotmail.com>
2013-03-25 23:47:59 +0800

    Drafted/Incomplete GUI for HRS (waiting for Kaye's version)

---
+++ HRS/src/hrs/entity/Guest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrs.entity;
+
+/**
+ *
+ * @author Kim
+ */
+public class Guest {
+    
+}
